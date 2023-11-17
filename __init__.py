966776841942ee2617be6f245cc9b6c93fed47a8
Aaron Moon <aaron@LAPTOP-6BT2VMKI.localdomain>
2023-11-17 00:00:50 -0800

    teams after date func

--- __init__.py
+++ __init__.py
@@ -1,1 +1,14 @@
+import requests
+import pandas as pd
+from tqdm import tqdm_notebook
+from time import sleep
+import os
+import json
+import sklearn
+from IPython.display import clear_output
 
+#get games played by a certain team after a certain date
+def get_team_games(df, date, team):
+    df['date'] = pd.to_datetime(df['date'])
+    filtered_df = df[(df['date'] >= date) & ((df['home_team_full_name'] == team) | (df['visitor_team_full_name'] == team))]
+    return filtered_df
